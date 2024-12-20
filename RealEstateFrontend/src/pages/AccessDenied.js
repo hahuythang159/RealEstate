88345e722a6ea87db6e10466b4a6880d8b04b450
hahuythang <hahuythang159@gmail.com>
2024-12-20 09:56:21 +0700

    chore(frontend): upgrade access denied page

--- RealEstateFrontend/src/pages/AccessDenied.js
+++ RealEstateFrontend/src/pages/AccessDenied.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FaUserLock } from 'react-icons/fa';
+import '../styles/AccessDenied.css';
 
 const AccessDenied = () => {
   return (
-    <div>
-      <h1>Access Denied</h1>
-      <p>You do not have permission to access this page.</p>
+    <div className="container">
+      <FaUserLock className="icon" />
+      <h1 className="title-ad">ACCESS DENIED</h1>
+      <p className="message">You do not have permission to access this page.</p>
     </div>
   );
 };
